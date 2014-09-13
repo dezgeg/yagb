f1c1017a7143159771241e5bf0e927b2f57329a5
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2014-09-13 07:59:38 +0300

    Texture-based renderer works!

--- gui/LcdWidget.cpp
+++ gui/LcdWidget.cpp
@@ -13,30 +13,35 @@
 
 enum Attr {
     Vertex,
-    TexCoord,
+};
+
+static const QVector2D vertexes[] = {
+        QVector2D(-1, -1), // bottom left
+        QVector2D(+1, -1), // bottom right
+        QVector2D(-1, +1), // top left
+        QVector2D(+1, +1), // top right
 };
 
 void LcdWidget::initializeGL() {
     glGetError();
-    glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
     glEnable(GL_TEXTURE_2D);
 
     texture.setSize(ScreenWidth, ScreenHeight);
     texture.setFormat(QOpenGLTexture::R8_UNorm);
     texture.setMipLevels(1);
+    texture.setMinMagFilters(QOpenGLTexture::Nearest, QOpenGLTexture::Nearest);
     texture.allocateStorage();
     texture.create();
 
     vertexShader = new QGLShader(QGLShader::Vertex, this);
     const char* vsrc =
             "attribute highp vec2 vertex;\n" \
-            "attribute mediump vec2 texCoord;\n" \
             "varying mediump vec2 texc;\n" \
             "void main(void)\n" \
             "{\n" \
             "    gl_Position = vec4(vertex, 0.0, 1.0);\n" \
-            "    texc = (vertex + vec2(1.0, 1.0)) / 2.0;\n" \
+            "    texc = vec2(1.0, -1.0) * (vertex + vec2(1.0, 1.0)) / 2.0;\n" \
             "}\n";
     vertexShader->compileSourceCode(vsrc);
 
@@ -46,9 +51,9 @@ void LcdWidget::initializeGL() {
             "varying mediump vec2 texc;\n" \
             "void main(void)\n" \
             "{\n" \
-            "    vec4 colorIndex = texture2D(texture, texc);\n" \
-            "    gl_FragColor = vec4(colorIndex.r/3.0, colorIndex.r/3.0, colorIndex.r/3.0, 1.0);\n" \
-            "    gl_FragColor = vec4(0.0, texc.s, texc.t, 1.0);\n" \
+            "    float index = texture2D(texture, texc).r * 255.0;\n" \
+            "    float grayScale = 1.0 - index/3.0;\n" \
+            "    gl_FragColor = vec4(grayScale, grayScale, grayScale, 1.0);\n" \
             "}\n";
     fragmentShader->compileSourceCode(fsrc);
 
@@ -56,49 +61,30 @@ void LcdWidget::initializeGL() {
     shaderProgram->addShader(vertexShader);
     shaderProgram->addShader(fragmentShader);
     shaderProgram->bindAttributeLocation("vertex", Attr::Vertex);
-    shaderProgram->bindAttributeLocation("texCoord", Attr::TexCoord);
     shaderProgram->link();
     if (!shaderProgram->isLinked()) {
         throw "Shader compiling/linking failed";
     }
 
     shaderProgram->bind();
-    shaderProgram->setUniformValue("texture", texture.textureId());
-}
-void LcdWidget::resizeGL(int w, int h) {
-    glViewport(0, 0, w, h);
-}
+    shaderProgram->setUniformValue("texture", 0);
 
-static const QVector2D vertexes[] = {
-        QVector2D(-1, -1), // bottom left
-        QVector2D(+1, -1), // bottom right
-        QVector2D(-1, +1), // top left
-        QVector2D(+1, +1), // top right
-};
-static const QVector2D texCoords[] = {
-        QVector2D(0, 0), // bottom left
-        QVector2D(1, 0), // bottom right
-        QVector2D(0, 1), // top left
-        QVector2D(1, 1), // top right
-};
-
-void LcdWidget::paintGL() {
-    glClearColor(0, 1, 0, 0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    shaderProgram->enableAttributeArray(Attr::Vertex);
+    CHECK_GL();
+    shaderProgram->setAttributeArray (Attr::Vertex, &vertexes[0]);
     CHECK_GL();
 
     texture.bind();
     CHECK_GL();
 
+}
+void LcdWidget::resizeGL(int w, int h) {
+    glViewport(0, 0, w, h);
+}
+
+void LcdWidget::paintGL() {
     texture.setData(QOpenGLTexture::Red, QOpenGLTexture::UInt8, (void*)framebuffer);
     CHECK_GL();
-
-    shaderProgram->enableAttributeArray(Attr::Vertex);
-    CHECK_GL();
-    shaderProgram->enableAttributeArray(Attr::TexCoord);
-    shaderProgram->setAttributeArray (Attr::Vertex, &vertexes[0]);
-    CHECK_GL();
-    shaderProgram->setAttributeArray (Attr::TexCoord, &texCoords[0]);
 
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
     CHECK_GL();
