dezgeg/yagb

commit cf91ee765781319b98171da5e7df9e2e5596cff9

Switch all shaders to unsigned ints

shaders/main.frag
@@ -1,8 +1,16 @@
-uniform sampler2D texture;
+#version 130
+
+uniform usampler2D texture;
 varying highp vec2 texc;
 
+uniform int textureHeight;
+uniform int textureWidth;
+
 void main(void) {
-    float index = texture2D(texture, texc).r * 255.0;
+    uint x = uint(round(texc.x / 2 * uint(textureWidth - 1)));
+    uint y = uint(round(texc.y / 2 * uint(textureHeight - 1)));
+
+    uint index = texelFetch(texture, ivec2(x, y), 0).r;
     float grayScale = 1.0 - index/3.0;
     gl_FragColor = vec4(grayScale, grayScale, grayScale, 1.0);
 }

gui/LcdWidget.cpp
@@ -25,7 +25,7 @@ QGLFormat& LcdWidget::createGLFormat() {
 }
 
 void LcdWidget::setupTexture(QOpenGLTexture& glTexture) {
-    glTexture.setFormat(QOpenGLTexture::R8_UNorm);
+    glTexture.setFormat(QOpenGLTexture::R8U);
     glTexture.setMipLevels(1);
     glTexture.setMinMagFilters(QOpenGLTexture::Nearest, QOpenGLTexture::Nearest);
     glTexture.allocateStorage();
@@ -111,8 +111,10 @@ void LcdWidget::initializeGL() {
     glActiveTexture(GL_TEXTURE0);
     CHECK_GL();
 
-    shaderProgram->setUniformValue("textureWidth", size().width());
-    shaderProgram->setUniformValue("textureHeight", size().height());
+    shaderProgram->setUniformValue("textureWidth", (GLint)size().width());
+    CHECK_GL();
+    shaderProgram->setUniformValue("textureHeight", (GLint)size().height());
+    CHECK_GL();
 }
 
 void LcdWidget::resizeGL(int w, int h) {
@@ -123,7 +125,7 @@ void LcdWidget::paintGL() {
     if (drawCallback) {
         drawCallback(this);
     }
-    texture.setData(QOpenGLTexture::Red, QOpenGLTexture::UInt8, (void*)textureData);
+    texture.setData(QOpenGLTexture::Red_Integer, QOpenGLTexture::UInt8, (void*)textureData);
     CHECK_GL();
 
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);

shaders/patternViewer.frag
@@ -1,6 +1,6 @@
 #version 130
 
-uniform sampler2D texture;
+uniform usampler2D texture;
 uniform sampler2D xAxisGrid;
 uniform sampler2D yAxisGrid;
 varying highp vec2 texc;
@@ -19,8 +19,8 @@ void main(void) {
 
     uint offs = 16u * (16u * tileY + tileX);
 
-    uint lsbs = uint(texelFetch(texture, ivec2((offs + 2u * bitY), 0.0), 0).r * 255.0);
-    uint msbs = uint(texelFetch(texture, ivec2((offs + 2u * bitY + 1u), 0.0), 0).r * 255.0);
+    uint lsbs = texelFetch(texture, ivec2((offs + 2u * bitY), 0.0), 0).r;
+    uint msbs = texelFetch(texture, ivec2((offs + 2u * bitY + 1u), 0.0), 0).r;
 
     uint b0 = (lsbs >> (7u - bitX)) & 1u;
     uint b1 = (msbs >> (7u - bitX)) & 1u;

shaders/tilemapViewer.frag
@@ -1,6 +1,6 @@
 #version 130
 
-uniform sampler2D texture;
+uniform usampler2D texture;
 uniform sampler2D xAxisGrid;
 uniform sampler2D yAxisGrid;
 varying highp vec2 texc;
@@ -12,30 +12,30 @@ uniform int textureHeight;
 uniform int textureWidth;
 
 void main(void) {
-    int patternOff = bgPatternBaseSelect != 0 ? 0x0 : 0x1000;
-    int tileOff = bgTileBaseSelect != 0 ? 0x1c00 : 0x1800;
+    uint patternOff = bgPatternBaseSelect != 0 ? 0x0u : 0x1000u;
+    uint tileOff = bgTileBaseSelect != 0 ? 0x1c00u : 0x1800u;
 
-    int x = int(round(texc.x * (textureWidth - 1)));
-    int y = int(round(texc.y * (textureHeight - 1)));
+    uint x = uint(round(texc.x * uint(textureWidth - 1)));
+    uint y = uint(round(texc.y * uint(textureHeight - 1)));
 
-    int tileX = x / 17;
-    int tileY = y / 17;
-    int bitX = (x % 17) / 2;
-    int bitY = (y % 17) / 2;
+    uint tileX = x / 17u;
+    uint tileY = y / 17u;
+    uint bitX = (x % 17u) / 2u;
+    uint bitY = (y % 17u) / 2u;
 
-    int rawTile = int(round(texture2D(texture, vec2((tileOff + 32 * tileY + tileX) / 8191.0, 0.0)).r * 255.0));
-    int tile = (bgPatternBaseSelect != 0) ? rawTile : (rawTile < 128 ? rawTile : -(256 - rawTile));
-    int offs = patternOff + 16 * tile;
+    uint rawTile = texelFetch(texture, ivec2(tileOff + 32u * tileY + tileX, 0.0), 0).r;
+    int tile = (bgPatternBaseSelect != 0) ? int(rawTile) : (rawTile < 128u ? int(rawTile) : -(256 - int(rawTile)));
+    uint offs = uint(int(patternOff) + 16 * tile);
 
-    int lsbs = int(texelFetch(texture, ivec2((offs + 2 * bitY), 0.0), 0).r * 255.0);
-    int msbs = int(texelFetch(texture, ivec2((offs + 2 * bitY + 1), 0.0), 0).r * 255.0);
+    uint lsbs = texelFetch(texture, ivec2((offs + 2u * bitY), 0.0), 0).r;
+    uint msbs = texelFetch(texture, ivec2((offs + 2u * bitY + 1u), 0.0), 0).r;
 
-    int b0 = (lsbs >> (7 - bitX)) & 1;
-    int b1 = (msbs >> (7 - bitX)) & 1;
+    uint b0 = (lsbs >> (7u - bitX)) & 1u;
+    uint b1 = (msbs >> (7u - bitX)) & 1u;
 
-    float grayScale = 1.0 - float(2 * b1 + b0)/3.0;
+    float grayScale = 1.0 - float(2u * b1 + b0)/3.0;
 
-    if (x % 17 == 16 || y % 17 == 16) {
+    if (x % 17u == 16u || y % 17u == 16u) {
         gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
     } else {
         gl_FragColor = vec4(grayScale, grayScale, grayScale, 1.0);

commit 15e79d3b81ae8809e3ef640c6a5618e9f0e8c9a7

Fix top-of-screen off-by-one
Fixes: cf91ee765781 ("Switch all shaders to unsigned ints")

shaders/main.frag
@@ -7,8 +7,8 @@ uniform int textureHeight;
 uniform int textureWidth;
 
 void main(void) {
-    uint x = uint(round(texc.x / 2 * uint(textureWidth - 1)));
-    uint y = uint(round(texc.y / 2 * uint(textureHeight - 1)));
+    uint x = uint(texc.x * uint(textureWidth - 1)) / 2u;
+    uint y = uint(texc.y * uint(textureHeight - 1)) / 2u;
 
     // HACK for passing 16-bit RGB via a byte buffer...
     uint index = texelFetch(texture, ivec2(2u * x, y), 0).r;